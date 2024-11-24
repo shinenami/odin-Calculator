b5da3292e2939aa2b37049de2ecd2cb76f07987c
shinenami <102074489+shinenami@users.noreply.github.com>
2024-11-24 14:17:24 +0300

    add clear, backspace. add listener for buttons

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ function divide(a, b) {
     return a / b
 }
 
-let inputA = null
-let inputB = null
-let inputOperator = null
+const inputA = document.getElementById("inputA")
+const inputB = document.getElementById("inputB")
+const inputOperator = document.getElementById("operator")
+const buttons = document.getElementById("buttons")
+
+inputA.textContent = "1234"
 
 function operate(A, operator = '+', B = A) {
     return operator == '+' ? sum(A, B) :
@@ -23,3 +26,49 @@ function operate(A, operator = '+', B = A) {
     sum(A, A)
 }
 
+function changeDisplay(button) {
+
+}
+
+function clear() {
+    inputA.textContent = '0'
+    inputOperator.textContent = ''
+    inputB.textContent = ''
+}
+
+function backspace() {
+    if (inputB.textContent != '') {
+        inputB.textContent = inputB.textContent.slice(0, -1)
+    }
+    if (inputOperator.textContent != '') {
+        inputOperator.textContent = inputOperator.textContent.slice(0, -1)
+    }
+    if (inputA.textContent != '') {
+        inputA.textContent = inputA.textContent.slice(0, -1)
+    }
+    
+    if (inputA.textContent.length == 0) {
+        inputA.textContent = '0'
+    }
+}
+
+buttons.addEventListener("click", (e) => {
+    if (e.target.classList.contains('number')) {
+        
+    }
+    if (e.target.classList.contains('operator')) {
+        
+    }
+    if (e.target.classList.contains('utility')) {
+        switch (e.target.id) {
+            case 'clear':
+                clear()
+                break;
+            case 'backspace':
+                backspace()
+                console.log('back')
+                break;
+
+        }
+    }
+})
